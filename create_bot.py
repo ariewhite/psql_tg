fc89f5e0087514e402f54347c5aa72362d3b009b
Chingiz <81185219+ariewhite@users.noreply.github.com>
2022-10-31 19:48:22 +0300

    Update create_bot.py

--- create_bot.py
+++ create_bot.py
@@ -1,6 +1,6 @@
 from aiogram import Bot, Dispatcher
 
-TOKEN = "5313684236:AAHyKUUsIWf8F3GNmiy20QSglnIV6SytFas"
+TOKEN = ":AAHyKUUsIWf8F3GNmiy20QSglnIV6SytFas"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot=bot)
